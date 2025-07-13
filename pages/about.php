8c963662df20deeacb1c1c39ef0f64dca0958f66
klow <chloe.carbs@gmail.com>
2025-07-13 23:20:13 +0800

    changed about

--- pages/about.php
+++ pages/about.php
@@ -1,6 +1,11 @@
 <?php include '../includes/header.php'; ?>
 <link rel="stylesheet" href="../assets/css/about.css">
 
+<div class="about-image-container">
+  <img src="../assets/images/index/MANILA.jpg" alt="Manila Skyline">
+  <div class="overlay-text">MANILA</div>
+</div>
+
 <div class="about-section">
   <h2>About StepIntoManila</h2>
   <p>
@@ -10,9 +15,4 @@
     visitor or a local explorer, StepIntoManila helps you step into the city with confidence,
     convenience, and a sense of wonder.
   </p>
-</div>
-
-<div class="about-image-container">
-  <img src="../assets/images/index/MANILA.jpg" alt="Manila Skyline">
-  <div class="overlay-text">MANILA</div>
 </div>
